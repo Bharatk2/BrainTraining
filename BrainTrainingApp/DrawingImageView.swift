5be9219e992f8618ebdb5e3f99e1df65282d1303
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-19 19:29:13 -0700

    Added the BrainTraining Project

---
+++ BrainTrainingApp/DrawingImageView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawingImageView.swift
+//  BrainTrainingApp
+//
+//  Created by Bhawnish Kumar on 6/19/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class DrawingImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
