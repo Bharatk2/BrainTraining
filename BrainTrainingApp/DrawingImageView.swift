9de6c62a38b722bb4298a4b59292bb58d6bf53c1
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-19 19:40:32 -0700

    Added the current position from where touch begins

--- BrainTrainingApp/DrawingImageView.swift
+++ BrainTrainingApp/DrawingImageView.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class DrawingImageView: UIImageView {
 
+    weak var delegate: ViewController?
+    var currentTouchPosition: CGPoint?
+
+    func draw(from start: CGPoint, to end: CGPoint) {
+        let renderer = UIGraphicsImageRenderer(size: bounds.size)
+        image = renderer.image { ctx in
+                image?.draw(in: bounds)
+
+                UIColor.black.setStroke()
+                ctx.cgContext.setLineCap(.round)
+                ctx.cgContext.setLineWidth(15)
+
+                ctx.cgContext.move(to: start)
+                ctx.cgContext.addLine(to: end)
+                ctx.cgContext.strokePath()
+            }
+
+     
+    }
+    
+    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        currentTouchPosition = touches.first?.location(in: self)
+        
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
