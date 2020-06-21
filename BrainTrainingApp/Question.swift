712674f1676effa8c7e17ce0626ef9be5be41090
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-21 12:54:17 -0700

    Added tableview cell methods to the view controller

---
+++ BrainTrainingApp/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  BrainTrainingApp
+//
+//  Created by Bhawnish Kumar on 6/21/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    
+    var text: String
+    var correctAnswer: Int
+    var actual: Int?
+}
+
+
--- BrainTrainingApp/ViewController.swift
+++ BrainTrainingApp/ViewController.swift
@@ -10,15 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var imageView: DrawingImageView!
+    
+    var questions = [Question]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+
     }
 
 
     func numberDrawn(_ image: UIImage) {
         
     }
+    
+    func setText(for cell: UITableViewCell, at indexPath: IndexPath, to question: Question) {
+        if indexPath.row == 0 {
+            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 48)
+             } else {
+                    cell.textLabel?.font = UIFont.systemFont(ofSize: 17)
+                }
 
+                if let actual = question.actual {
+                    cell.textLabel?.text = "\(question.text) = \(actual)"
+                } else {
+                    cell.textLabel?.text = "\(question.text) = ?"
+                }
+            }
+
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let currentQuestion = questions[indexPath.row]
+        setText(for: cell, at: indexPath, to: currentQuestion)
+    return cell
+        
+    }
+    
+   
+    
+    
 }
 
